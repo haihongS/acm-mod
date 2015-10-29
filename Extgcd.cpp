6c4052961ff5be6456b57975345ac9b89b8494c3
haihongs <shh_name@hotmail.com>
2015-10-29 20:47:42 +0800

    Create Extgcd.cpp

---
+++ Extgcd.cpp
@@ -0,0 +1,22 @@
+/*
+扩展欧几里得算法
+a,b两数,a>b, 存在x,y使 a*x+b*y=gcd(a,b)
+x,y传值引用
+函数返回值为gcd(a,b)
+*/
+int Extgcd(int a,int b,int & x,int & y)
+{
+    if(b==0)
+    {
+        x=1;y=0;
+        return a;
+    }
+    else
+    {
+        int ans=Extgcd(b,a%b,x,y);
+        int t=x;
+        x=y;
+        y=t-(a/b)*y;
+        return ans;
+    }
+}
