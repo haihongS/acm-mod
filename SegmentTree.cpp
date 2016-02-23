9a7f3853c4fe9ea3564f29f650eaac17f5706c46
haihongs <shh_name@hotmail.com>
2016-02-23 11:50:47 +0800

    Added files via upload

---
+++ SegmentTree.cpp
@@ -0,0 +1,32 @@
+/* ***********************************************
+Author        :shootPlane
+Created Time  :2016/2/5 20:45:31
+File Name     :S:\01\main.cpp
+blog:http://haihongblog.com
+************************************************ */
+
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <vector>
+#include <queue>
+#include <set>
+#include <map>
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include <ctime>
+#include <stack>
+using namespace std;
+
+int main()
+{
+    //freopen("in.txt","r",stdin);
+    //freopen("out.txt","w",stdout);
+    int a;
+	cout<<"input\n";
+	cin>>a;
+	cout<<a;
+    return 0;
+}
