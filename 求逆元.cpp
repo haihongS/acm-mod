93e35174b4505828754bd0dc45e6680a1a8c799b
haihongs <shh_name@hotmail.com>
2016-08-04 14:30:43 +0800

    Create 求逆元.cpp

---
+++ 求逆元.cpp
@@ -0,0 +1,17 @@
+long long extend_gcd(long long a,long long b,long long &x,long long &y)
+{
+    if(a==0&&b==0) return -1;//无最大公约数
+    if(b==0){x=1;y=0;return a;}
+    long long d=extend_gcd(b,a%b,y,x);
+    y-=a/b*x;
+    return d;
+}
+//*********求逆元素*******************
+//ax = 1(mod n)
+long long mod_reverse(long long a,long long n)
+{
+    long long x,y;
+    long long d=extend_gcd(a,n,x,y);
+    if(d==1) return (x%n+n)%n;
+    else return -1;
+}
