1fd88ba25d7e121eaf710626b98a3789b894e2a8
haihongs <shh_name@hotmail.com>
2016-09-19 13:21:17 +0800

    Update Lucas.cpp

--- Lucas.cpp
+++ Lucas.cpp
@@ -3,6 +3,45 @@
 先调init初始化
 */
 
+//(2)
+const int N = 1000005;
+
+ll fac[N];
+
+void init()//预处理阶乘
+{
+    fac[0]=1;
+    for(int i=1;i<=N;i++)
+        fac[i]=i*fac[i-1]%mod;
+}
+ll pow_mod(ll a,ll b)
+{
+    ll s=1;
+    a=a%mod;
+    while(b)
+    {
+        if(b&1)
+            s=s*a%mod;
+        a=a*a%mod;
+        b>>=1;
+    }
+    return s;
+}
+ll C(int n,int m)
+{
+    if(m>n)
+        return 0;
+    return  fac[n]*pow_mod(fac[m]*fac[n-m]%mod,mod-2)%mod;
+}
+ll Lucas(int n,int m)
+{
+    if(m==0)
+        return 1;
+    return C(n%mod,m%mod)*Lucas(n/mod,m/mod)%mod;
+}
+
+
+// (1)
 const ll MOD=110119;
 ll f[MOD+10],all[MOD+10];
 
